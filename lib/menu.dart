17ac15ed2641a05e38e57248dccbdd000eb9ce77
NOUR <noureldeenderna@gmail.com>
2023-04-25 21:12:52 +0200

    Fix Profile +1

--- lib/menu.dart
+++ lib/menu.dart
@@ -4,6 +4,7 @@ import 'package:dawerf/HomeScreen/Containers.dart';
 import 'package:dawerf/HomeScreen/dawer_home.dart';
 import 'package:dawerf/Notification/notification_screen.dart';
 import 'package:dawerf/Profile/Profile.dart';
+import 'package:dawerf/Profile/Profile2.dart';
 import 'package:dawerf/report.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
@@ -23,7 +24,7 @@ class BottomNavBar extends StatelessWidget {
         Containers(),
         ReportScreen(),
         NotificationScreen(),
-        Profile(),
+        Profile2(),
 
 
       ];
